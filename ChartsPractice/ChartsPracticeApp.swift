47bf9352798a9878a3807a0b906b3b974ae1925a
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-17 13:28:42 +0900

    Initial Commit

---
+++ ChartsPractice/ChartsPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsPracticeApp.swift
+//  ChartsPractice
+//
+//  Created by 渡邊魁優 on 2023/02/17.
+//
+
+import SwiftUI
+
+@main
+struct ChartsPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
